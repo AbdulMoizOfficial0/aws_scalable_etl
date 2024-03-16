fbabcc45d32e04947322f6796a5e94ec089ab0ce
Abdul Moiz <abdulmoizofficial0@gmail.com>
2024-03-17 03:45:20 +0500

    done

--- scalable_etl/main.py
+++ scalable_etl/main.py
@@ -5,6 +5,7 @@ from data_transformation.mysql_transformer import MySQLTransformer
 from data_loading.data_loading import MySQLLoader
 from config.config import ETLConfig
 
+
 def main():
     csv_extractor = CSVExtractor(ETLConfig.CSV_DATA_PATH)
     mysql_extractor = MySQLExtractor()
@@ -15,9 +16,10 @@ def main():
     csv_transformed_data = CSVTransformation.transform(csv_data)
     mysql_transformed_data = MySQLTransformer.transform_data(mysql_data)
 
-    MySQLLoader.load_data(mysql_transformed_data)
+    MySQLLoader.load_data(mysql_transformed_data + csv_transformed_data)
 
     return csv_transformed_data, mysql_transformed_data
+
 
 if __name__ == "__main__":
     main()
